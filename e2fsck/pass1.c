openharmony/third_party_e2fsprogs

commit 9dd52068aa9b482ea8d60a0e09a0fc294c9750ed

e2fsck: fix logic for deciding when to repair legacy timestamp encodings
A static code checker noticed that we had a redundant condition:

    if (((sizeof(time_t) <= 4) ||
         ((sizeof(time_t) > 4) &&

which was caused by the parenthesis were in the wrong place.

Signed-off-by: Theodore Ts'o <tytso@mit.edu>

e2fsck/pass1.c
@@ -512,8 +512,8 @@ static void check_inode_extra_space(e2fsck_t ctx, struct problem_context *pctx)
 	 * If the inode's extended atime (ctime, crtime, mtime) is stored in
 	 * the old, invalid format, repair it.
 	 */
-	if (((sizeof(time_t) <= 4) ||
-	     ((sizeof(time_t) > 4) &&
+	if ((sizeof(time_t) <= 4) ||
+	    (((sizeof(time_t) > 4) &&
 	      ctx->now < EXT4_EXTRA_NEGATIVE_DATE_CUTOFF)) &&
 	    (CHECK_INODE_EXTRA_NEGATIVE_EPOCH(inode, atime) ||
 	     CHECK_INODE_EXTRA_NEGATIVE_EPOCH(inode, ctime) ||

commit 1dfdbadf11aceebf9fc9da71c2120deef20b3cbe

e2fsck: fix timestamps logic for 32-bit systems
Commit 9dd52068aa9b4 introduced a regression which caused e2fsck on
32-bit systems to think all timestamps were legacy pre-1970
timestamps.  Fix the bug.

Signed-off-by: Theodore Ts'o <tytso@mit.edu>

e2fsck/pass1.c
@@ -512,9 +512,9 @@ static void check_inode_extra_space(e2fsck_t ctx, struct problem_context *pctx)
 	 * If the inode's extended atime (ctime, crtime, mtime) is stored in
 	 * the old, invalid format, repair it.
 	 */
-	if ((sizeof(time_t) <= 4) ||
-	    (((sizeof(time_t) > 4) &&
-	      ctx->now < EXT4_EXTRA_NEGATIVE_DATE_CUTOFF)) &&
+	if (((sizeof(time_t) <= 4) ||
+	     (((sizeof(time_t) > 4) &&
+	       ctx->now < EXT4_EXTRA_NEGATIVE_DATE_CUTOFF))) &&
 	    (CHECK_INODE_EXTRA_NEGATIVE_EPOCH(inode, atime) ||
 	     CHECK_INODE_EXTRA_NEGATIVE_EPOCH(inode, ctime) ||
 	     CHECK_INODE_EXTRA_NEGATIVE_EPOCH(inode, crtime) ||