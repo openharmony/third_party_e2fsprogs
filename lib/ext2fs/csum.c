openharmony/third_party_e2fsprogs

commit c174d7e95be800cb88314926e40df8cbd1ca2229

libext2fs: make ext2fs_dirent_has_tail() more strict
Previously ext2fs_dirent_has_tail() would return true if the directory
was corrupted.  If the directory is corrupted, then by definition it
doesn't have a valid checksum tail.

(This fixes a big-endian failure on the master branch.)

Signed-off-by: Theodore Ts'o <tytso@mit.edu>

lib/ext2fs/csum.c
@@ -267,13 +267,15 @@ static errcode_t __get_dirent_tail(ext2_filsys fs,
 	top = EXT2_DIRENT_TAIL(dirent, fs->blocksize);
 
 	rec_len = translate(d->rec_len);
-	while (rec_len && !(rec_len & 0x3)) {
+	while ((void *) d < top) {
+		if ((rec_len < 8) || (rec_len & 0x03))
+			return EXT2_ET_DIR_CORRUPTED;
 		d = (struct ext2_dir_entry *)(((char *)d) + rec_len);
-		if ((void *)d >= top)
-			break;
 		rec_len = translate(d->rec_len);
 	}
 
+	if ((void *)d > ((void *)dirent + fs->blocksize))
+			return EXT2_ET_DIR_CORRUPTED;
 	if (d != top)
 		return EXT2_ET_DIR_NO_SPACE_FOR_CSUM;
 
@@ -290,7 +292,8 @@ static errcode_t __get_dirent_tail(ext2_filsys fs,
 
 int ext2fs_dirent_has_tail(ext2_filsys fs, struct ext2_dir_entry *dirent)
 {
-	return __get_dirent_tail(fs, dirent, NULL, 0) == 0;
+	return __get_dirent_tail(fs, dirent, NULL, 0) !=
+		EXT2_ET_DIR_NO_SPACE_FOR_CSUM;
 }
 
 static errcode_t ext2fs_dirent_csum(ext2_filsys fs, ext2_ino_t inum,

tests/f_dir_bad_csum/expect.1
@@ -9,20 +9,14 @@ Fix? yes
 Directory inode 14, block #0, offset 0: directory has no checksum.
 Fix? yes
 
-Directory inode 15, block #0, offset 0: directory has no checksum.
-Fix? yes
-
 Directory inode 15, block #0, offset 1000: directory corrupted
 Salvage? yes
 
-Directory inode 16, block #0, offset 0: directory has no checksum.
+Entry '' in ??? (15) has rec_len of 0, should be 12.
 Fix? yes
 
 Directory inode 16, block #0, offset 12: directory corrupted
 Salvage? yes
-
-Directory inode 17, block #0, offset 0: directory has no checksum.
-Fix? yes
 
 Directory inode 17, block #0, offset 0: directory corrupted
 Salvage? yes

tests/f_rebuild_csum_rootdir/expect.1
@@ -1,8 +1,5 @@
 Pass 1: Checking inodes, blocks, and sizes
 Pass 2: Checking directory structure
-Directory inode 2, block #0, offset 0: directory has no checksum.
-Fix? yes
-
 Directory inode 2, block #0, offset 0: directory corrupted
 Salvage? yes
 

tests/f_resize_inode_meta_bg/expect.1
@@ -5,9 +5,6 @@ Resize_inode not enabled, but the resize inode is non-zero.  Clear? yes
 
 Pass 1: Checking inodes, blocks, and sizes
 Pass 2: Checking directory structure
-Directory inode 2, block #0, offset 0: directory has no checksum.
-Fix? yes
-
 First entry '' (inode=348) in directory inode 2 (???) should be '.'
 Fix? yes
 
@@ -18,6 +15,9 @@ Fix? yes
 Setting filetype for entry '..' in ??? (2) to 2.
 Directory inode 2, block #0, offset 860: directory corrupted
 Salvage? yes
+
+Entry '' in ??? (2) has rec_len of 0, should be 12.
+Fix? yes
 
 Directory inode 11, block #0, offset 0: directory corrupted
 Salvage? yes
@@ -41,6 +41,9 @@ Clear? yes
 
 Directory inode 11, block #3, offset 864: directory corrupted
 Salvage? yes
+
+Entry '' in ??? (11) has rec_len of 0, should be 12.
+Fix? yes
 
 Pass 3: Checking directory connectivity
 '..' in / (2) is <The NULL inode> (0), should be / (2).

commit 099e96bba4491974c5eded208db8ab5a90028b87

libext2fs: fix potential buffer overrun in __get_dirent_tail()
If the file system is corrupted, there is a potential of a read-only
buffer overrun.  Fortunately, we don't actually use the result of that
pointer dereference, and the overrun is at most 64k.

Google-Bug-Id: #158564737
Fixes: c174d7e95be8 ("libext2fs: make ext2fs_dirent_has_tail() more strict")
Signed-off-by: Theodore Ts'o <tytso@mit.edu>

lib/ext2fs/csum.c
@@ -266,12 +266,11 @@ static errcode_t __get_dirent_tail(ext2_filsys fs,
 	d = dirent;
 	top = EXT2_DIRENT_TAIL(dirent, fs->blocksize);
 
-	rec_len = translate(d->rec_len);
 	while ((void *) d < top) {
+		rec_len = translate(d->rec_len);
 		if ((rec_len < 8) || (rec_len & 0x03))
 			return EXT2_ET_DIR_CORRUPTED;
 		d = (struct ext2_dir_entry *)(((char *)d) + rec_len);
-		rec_len = translate(d->rec_len);
 	}
 
 	if ((char *)d > ((char *)dirent + fs->blocksize))