088127903bfea948a77e89931b1de49ca7e86c33
张文迪 <zhangwendi3@huawei.com>
2023-06-03 16:43:33 +0800

    fix e2fsprog compile
    Signed-off-by: 张文迪 <zhangwendi3@huawei.com>

--- install.sh
+++ install.sh
@@ -30,5 +30,4 @@ patch -p1 < $1/1002-add-header-file-to-musl-compile-mk2efs.patch --fuzz=0 --no-b
 patch -p1 < $1/1003-add-dac-config.patch --fuzz=0 --no-backup-if-mismatch
 patch -p1 < $1/1004-modify-code-to-compile.patch --fuzz=0 --no-backup-if-mismatch
 patch -p1 < $1/1005-read-vfat-chinese-label.patch --fuzz=0 --no-backup-if-mismatch
-patch -p1 < $1/1006-add-build-gn.patch --fuzz=0 --no-backup-if-mismatch
 exit 0
